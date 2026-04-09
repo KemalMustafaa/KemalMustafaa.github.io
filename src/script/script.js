9761aab4c65e93e3f6ae116b9ffa50540b40f04b
KemalMustafaa <mustafakemal1505@gmail.com>
2026-04-10 00:04:15 +0700

    menyelesaikan section service dan menambahkan section project

--- src/script/script.js
+++ src/script/script.js
@@ -13,3 +13,27 @@ typewriter
   .pauseFor(2500)
   .deleteAll()
   .start();
+
+// untuk membuat project li
+// Tambahkan titik (.) sebelum nama class
+const tag = document.querySelector("section.project");
+const projects = tag.querySelectorAll(".list");
+const descs = tag.querySelectorAll(".desc");
+
+projects.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    const isCurrentlyOpen = descs[i].classList.contains("h-fit");
+
+    // 1. Tutup SEMUA list terlebih dahulu
+    descs.forEach((el) => {
+      el.classList.remove("h-fit", "py-4");
+      el.classList.add("h-0");
+    });
+
+    // 2. Jika yang diklik tadi tidak sedang terbuka, maka buka
+    if (!isCurrentlyOpen) {
+      descs[i].classList.add("h-fit", "py-4");
+      descs[i].classList.remove("h-0");
+    }
+  });
+});
